87af1751268bc12ef449c8a3590a6462dbee96b7
charrus <charlie@rusbridger.com>
2021-06-16 20:46:12 +0100

    Better UKini and better compiler.

---
+++ clang12.sh
@@ -0,0 +1,6 @@
+export CC=/usr/bin/clang-12
+export CPP=/usr/bin/clang-cpp-12
+export CXX=/usr/bin/clang++-12
+export LD=/usr/bin/ld.lld-12
+export CFLAGS="-march=x86-64-v3"
+export CPPFLAGS="-march=x86-64-v3"
